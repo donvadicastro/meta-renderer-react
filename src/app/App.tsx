16bf0bd59818c983e262da89cb1f4dc2f9d3ddab
Vadym <vadim.barylo@intapp.com>
2021-10-29 15:19:21 +0300

    add forms selector

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,45 +2,84 @@ import React, {useState} from 'react';
 import './App.css';
 import {FormComponentWrapper} from "../components";
 import 'bootstrap/dist/css/bootstrap.css';
-import {mainFormDefinitionYAML} from "../definitions/mainFormYAML";
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-yaml";
 import "ace-builds/src-noconflict/theme-xcode";
 import {Ace} from "ace-builds";
 
+import {mainFormDefinitionYAML} from "../definitions/custom/mainFormYAML";
+import {dataBindingWithPrimitives} from "../definitions/dataBindingWithPrimitives.yml";
+import {dataBindingWithCollections} from "../definitions/dataBindingWithCollections.yml";
+import {actions} from "../definitions/actions.yml";
+import {dynamics} from "../definitions/dynamics.yml";
+import {layout} from "../definitions/layout.yml";
+
+
 const YAML = require('yamljs');
 
 function App() {
     const [value, setValue] = useState(mainFormDefinitionYAML);
 
-    let onBlur = function (event: any, editor?: Ace.Editor) {
+    function onBlur(event: any, editor?: Ace.Editor) {
         try {
             YAML.parse(editor?.getValue());
             editor && setValue(editor.getValue());
         } catch (e) {
             alert(e);
         }
-    };
+    }
+
+    function loadFile(yml: string) {
+        setValue({dataBindingWithPrimitives, dataBindingWithCollections, actions, dynamics, layout}[yml] || '');
+    }
 
     return (
-        <div className="container-fluid min-100 d-flex flex-column">
+        <div className="min-100 d-flex flex-column overflow-hidden">
+            <header className="site-header sticky-top py-2">
+                <nav className="container d-flex flex-md-row justify-content-between">
+                    <a className="py-2" href="#" aria-label="Product">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
+                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="d-block mx-auto"
+                             role="img" viewBox="0 0 24 24"><title>Product</title>
+                            <circle cx="12" cy="12" r="10"></circle>
+                            <path
+                                d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path>
+                        </svg>
+                    </a>
+                    <a className="py-2 d-none d-md-inline-block" onClick={() => loadFile('dataBindingWithPrimitives')} href="#">
+                        Data binding with primitives
+                    </a>
+                    <a className="py-2 d-none d-md-inline-block" onClick={() => loadFile('dataBindingWithCollections')} href="#">
+                        Data binding with collections
+                    </a>
+                    <a className="py-2 d-none d-md-inline-block" onClick={() => loadFile('dynamics')} href="#">
+                        Dynamics
+                    </a>
+                    <a className="py-2 d-none d-md-inline-block" onClick={() => loadFile('actions')} href="#">
+                        Actions
+                    </a>
+                    <a className="py-2 d-none d-md-inline-block" onClick={() => loadFile('layout')} href="#">
+                        Layout
+                    </a>
+                </nav>
+            </header>
             <div className="row flex-grow-1">
-                <div className="col-sm-4">
-                    <AceEditor
-                        mode="yaml"
-                        theme="xcode"
-                        value={value}
-                        width={"100%"}
-                        height={"100%"}
-                        onBlur={onBlur}
-                        name="FormYamlEditor"
-                        editorProps={{ $blockScrolling: true }} />
+                    <div className="col-sm-4">
+                        <AceEditor
+                            mode="yaml"
+                            theme="xcode"
+                            value={value}
+                            width={"100%"}
+                            height={"100%"}
+                            onBlur={onBlur}
+                            name="FormYamlEditor"
+                            editorProps={{ $blockScrolling: true }} />
+                    </div>
+                    <div className="col-sm-8 pt-2">
+                        <FormComponentWrapper yaml={value} />
+                    </div>
                 </div>
-                <div className="col-sm-8">
-                    <FormComponentWrapper yaml={value} />
-                </div>
-            </div>
         </div>
     );
 }
