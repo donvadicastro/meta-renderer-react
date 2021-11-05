9e795dd6565bca7b8a3b8cc379972e16c8456a51
Vadym <vadim.barylo@intapp.com>
2021-11-05 14:01:51 +0200

    support paginator, closes #4

--- src/components/data/tableComponent.tsx
+++ src/components/data/tableComponent.tsx
@@ -13,12 +13,17 @@ import 'primeicons/primeicons.css';
  */
 export class TableComponent extends DataComponentBase<CollectionBase> {
     render() {
+        let element = this.props.elementRef();
+        let paginator = element.getPropertyValue('ui.paginator');
+
         return (
             <div>
-                <DataTable value={this.props.elementRef().getValue()}
-                           header={this.props.elementRef().getPropertyValue('ui.label')}
+                <DataTable value={element.getValue()}
+                           header={element.getPropertyValue('ui.label')}
                            selectionMode="single"
-                           paginator rows={this.props.elementRef().getPropertyValue('ui.rows') || 10} rowsPerPageOptions={[10,25,50,100]}>
+                           paginator={paginator !== undefined ? paginator : true}
+                           rows={element.getPropertyValue('ui.rows') || 10}
+                           rowsPerPageOptions={[10,25,50,100]}>
                     {this.props.meta.items.map((x: any) => <Column field={x.binding} header={x.ui?.label || x.name}/>)}
                 </DataTable>
             </div>
